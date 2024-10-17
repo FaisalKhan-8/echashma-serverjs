0beb642d7b07802c8a60fb2d9b71054bdd856464
Faisal Khan <mdfaisalkhan1500@gmail.com>
2024-10-18 01:24:21 +0530

    cc

--- src/server.js
+++ src/server.js
@@ -1,35 +1,35 @@
-const express = require('express');
-const cors = require('cors');
-const portfinder = require('portfinder');
-const dotenv = require('dotenv');
-const path = require('path');
-const rootRouter = require('./routes/index');
-const { errorHandler } = require('./middleware/errors');
-const fs = require('fs');
+const express = require('express')
+const cors = require('cors')
+const portfinder = require('portfinder')
+const dotenv = require('dotenv')
+const path = require('path')
+const rootRouter = require('./routes/index')
+const { errorHandler } = require('./middleware/errors')
+const fs = require('fs')
 
-dotenv.config();
+dotenv.config()
 
 // Create Express application
-const app = express();
+const app = express()
 
 // Middleware setup
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // API routes
-app.use('/api', rootRouter);
+app.use('/api', rootRouter)
 
 // Resolve path for static files
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-const distPath = path.join(process.cwd(), '/src/dist');
-app.use(express.static(distPath));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
+const distPath = path.join(process.cwd(), '/src/dist')
+app.use(express.static(distPath))
 app.get('*', (req, res) => {
-  res.sendFile(path.join(distPath, 'index.html'));
-});
+  res.sendFile(path.join(distPath, 'index.html'))
+})
 
 // Error handling middleware
-app.use(errorHandler);
+app.use(errorHandler)
 
 // Function to start the server
 const startServer = async () => {
@@ -37,15 +37,15 @@ const startServer = async () => {
     // Find an available port
     const port = await portfinder.getPortPromise({
       port: parseInt(process.env.PORT || '8000', 10),
-    });
+    })
 
     app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
-    });
+      console.log(`Server is running on port: http://localhost:${port}`)
+    })
   } catch (error) {
-    console.error('Error starting the server:', error);
+    console.error('Error starting the server:', error)
   }
-};
+}
 
 // Start the server
-startServer();
+startServer()
