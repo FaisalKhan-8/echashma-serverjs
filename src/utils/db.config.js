e5a877e7aa6dedef194223a211721d5afa4c0cd5
Faisal Khan <mdfaisalkhan1500@gmail.com>
2024-10-25 22:16:00 +0530

    cc

--- src/utils/db.config.js
+++ src/utils/db.config.js
@@ -1,7 +1,5 @@
-const { PrismaClient } = require("@prisma/client");
+const { PrismaClient } = require('@prisma/client')
 
-const db = new PrismaClient({
-  log: ["query", "error"],
-});
+const db = new PrismaClient({})
 
-module.exports = db;
+module.exports = db
