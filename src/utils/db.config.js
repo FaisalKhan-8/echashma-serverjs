437652d0d11b9c54c1b35cbb1f14660ca81ec614
FaisalKhan-8 <mdfaisalkhan1500@gmail.com>
2024-11-08 20:01:46 +0530

    cc

--- src/utils/db.config.js
+++ src/utils/db.config.js
@@ -1,5 +1,16 @@
-const { PrismaClient } = require('@prisma/client')
+const { PrismaClient } = require('@prisma/client');
 
-const db = new PrismaClient({})
+let db;
 
-module.exports = db
+// In serverless environments, reuse the Prisma Client instance across function invocations
+if (process.env.NODE_ENV === 'production') {
+  db = new PrismaClient();
+} else {
+  // In development, we can safely reuse the Prisma Client instance in memory
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  db = global.prisma;
+}
+
+module.exports = db;
